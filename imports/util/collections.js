c7105effc49ab1b6a1885a1a98df298d0e1076ae
serafino <abraham.serafino@gmail.com>
2017-06-21 18:02:08 -0500

    Document-level permissions for 'find'

--- imports/util/collections.js
+++ imports/util/collections.js
@@ -31,105 +31,32 @@ function setupPublications(collection, collectionName) {
 
   Meteor.publish(collectionName, function() {
     if (checkCollectionPermissions.find(collection, CURRENT_USER_ID)) {
-      console.log('user has permission');
-      return collection.find(); // { /* _permissions: { [CURRENT_USER_ID]: { find: true } } */ });
+      return collection.find({ _permissions: {
+        [CURRENT_USER_ID]: { find: true }
+      } });
     } else {
       return this.ready();
     }
   });
 }
 
-function createMeteorMethods(collection, collectionName) {
+function createPermissionMethods(collection, collectionName) {
   // add or remove a permissions for a user
   ['add', 'remove'].forEach(method => {
     Meteor.methods({
       [`${collectionName}.${method}Permission`](userId, operation) {
         checkCollectionPermissions['change collection permissions'](collection, global.CURRENT_USER_ID);
-        collection.update({ _id: 'PERMISSIONS' }, {
+        collection.update({_id: 'PERMISSIONS'}, {
           $set: {
             [`${userId}.${operation}`]: method === 'add'
           }
-        }, { upsert: true });
+        }, {upsert: true});
       }
     });
 
     collection[`${method}Permission`] = (userId, operation) =>
-      Meteor.call(`${collectionName}.${method}Permission`, userId, operation);
+        Meteor.call(`${collectionName}.${method}Permission`, userId, operation);
   });
-
-  /* // findAll, removeAll (the 'All' is appended)
-  ['find', 'remove'].forEach(method => {
-    Meteor.methods({
-      [`${collectionName}.${method}All`]() {
-        if (!Meteor.isSimulation) {
-          checkPermissions[method === 'find' ? 'read' : 'remove']
-          (collection, global.CURRENT_USER_ID);
-
-          return Promise.await(collection[`${method}All`]());
-        }
-      }
-    });
-
-    api[`${method}All`] = (query) => new Promise((resolve, reject) =>
-      Meteor.call(`${collectionName}.${method}All`,
-        (err, res) => {
-          console.log('Hello world');
-          if (err) reject(err);
-          else resolve (res);
-        }));
-  });
-
-  // methods that require read permissions and accept
-  // a single param called 'query'
-  ['find', 'findOne', 'findById'].forEach(method => {
-    Meteor.methods({
-      [`${collectionName}.${method}`](query) {
-        checkPermissions.read(collection, global.CURRENT_USER_ID);
-        return collection[method](query);
-      }
-    });
-
-    api[method] = (query) => new Promise((resolve, rejest) =>
-      Meteor.call(`${collectionName}.${method}`, query,
-        (err, res) => {
-          if (err) reject(err);
-          else resolve (res);
-        }));
-  });
-
-  // the "remove" method (takes a single param called 'query')
-  Meteor.methods({
-    [`${collectionName}.remove`](query) {
-      checkPermissions.remove(collection, global.CURRENT_USER_ID);
-      collection.remove(query);
-    }
-  });
-
-  api.remove = (query) => new Promise((resolve, rejest) =>
-    Meteor.call(`${collectionName}.remove`, query,
-      (err, res) => {
-        if (err) reject(err);
-        else resolve (res);
-      }));
-
-  // the "save" method (takes a data model and optional query param)
-  Meteor.methods({
-    [`${collectionName}.save`](model, query) {
-      checkPermissions.save(collection, global.CURRENT_USER_ID);
-      collection.save(model, query);
-    }
-  });
-
-  api.save = (model, query) => new Promise((resolve, rejest) =>
-    Meteor.call(`${collectionName}.save`, model, query,
-      (err, res) => {
-        if (err) reject(err);
-        else resolve (res);
-      }));
-
-  api.originalCollection = collection;
-
-  return api; */
 }
 
 function createCollection(name, schema) {
@@ -169,7 +96,7 @@ function createCollection(name, schema) {
     Meteor.subscribe(name);
   }
 
-  createMeteorMethods(collection, name);
+  createPermissionMethods(collection, name);
 
   return collection;
 }
--- imports/util/generate-test-data.js
+++ imports/util/generate-test-data.js
@@ -14,7 +14,13 @@ export default function generateTestData() {
   Posts.addPermission('TEST_USER', 'update');
   Posts.addPermission('TEST_USER', 'remove');
 
-  /* for (let count = 3; count < 6; ++count) {
-    Posts.save({ text: `Post ${count}` });
-  } */
+  for (let count = 1; count < 4; ++count) {
+    Posts.insert({ text: `Post ${count}` });
+  }
+
+  for (let count = 3; count < 6; ++count) {
+    Posts.insert({ text: `Post ${count}`, _permissions: {
+      TEST_USER: { find: true }
+    } });
+  }
 }
