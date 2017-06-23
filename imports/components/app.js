6d00b8f1c41c29a46f6b6b51f2eaee7d52f3d5b1
serafino <abraham.serafino@gmail.com>
2017-06-22 22:56:09 -0500

    World permissions

--- imports/components/app.js
+++ imports/components/app.js
@@ -5,23 +5,24 @@ import { Posts } from '../api';
 import './app.html';
 
 let count = 0;
-let model = {
-  data: {}
-};
 
 rivets.components['app'] = {
   template: () => Blaze.toHTML(Template['app']),
   initialize() {
-    model.testMode = BlogJS.isTest ? 'on' : 'off';
+    const model = {
+      testMode: BlogJS.isTest ? 'on' : 'off',
 
-    model.addPost = () => {
-      Posts.save({ text: `Post ${count++}` });
+      addPost() {
+        Posts.save({ text: `Post ${count++}` });
+      },
+
+      posts: []
     };
 
     Tracker.autorun(() => {
-      model.data.posts = Posts.findAll().fetch();
+      model.posts = Posts.findAll().fetch();
     });
 
-    return model;
+    return { model };
   }
 };
--- imports/util/collections.js
+++ imports/util/collections.js
@@ -1,65 +1,139 @@
 import { Promise } from 'meteor/promise';
 import { Meteor } from 'meteor/meteor';
+import merge from 'lodash.merge';
 import { Mongo } from 'meteor/mongo';
 
 const checkCollectionPermissions = {};
 
-function checkPermission({ collection, userId, operation, collectionName }) {
-  if (Meteor.isServer) {
-    const permissions = collection.findOne({ _id: 'PERMISSIONS' });
-    const message = `UserId (${userId}) may not ${operation} on '${collectionName}'`;
+function initializePermissions(permissions, collectionName) {
+  function checkPermission({ collection, userId, operation, collectionName }) {
+    if (Meteor.isServer) {
+      const permissions = collection.findOne({ _id: 'PERMISSIONS' });
+      const message = `UserId (${userId}) may not ${operation} on '${collectionName}'`;
 
-    if (!permissions || ((!permissions[userId] || !permissions[userId][operation]) &&
-        (!permissions._world_ || !permissions._world_[operation]))) {
+      if (!permissions || ((!permissions[userId] || !permissions[userId][operation]) &&
+          (!permissions._world_ || !permissions._world_[operation]))) {
 
-      if (operation === 'find') {
-        return false;
+        if (operation === 'read') {
+          return false;
+        }
+
+        throw new Error(message);
       }
 
-      throw new Error(message);
+      return true;
     }
-
-    return true;
   }
-}
 
-function setupPublications(collection, collectionName) {
-  ['find', 'remove', 'update', 'insert', 'change collection permissions']
-      .forEach(operation => {
-
+  permissions.forEach(operation => {
     checkCollectionPermissions[operation] = (collection, userId) =>
         checkPermission({ collection, userId, operation, collectionName });
   });
+}
 
-  Meteor.publish(collectionName, function() {
-    if (checkCollectionPermissions.find(collection, CURRENT_USER_ID)) {
-      return collection.find({ $or: [
-        { _permissions: { [CURRENT_USER_ID]: { find: true } } },
-        { _permissions: { _world_: { find: true } } }
-      ] });
+function setupPublications(collection, collectionName) {
+  Meteor.publish(collectionName, function() { // cannot be an arrow function
+    if (checkCollectionPermissions.read(collection, CURRENT_USER_ID)) {
+      console.log(collection.find({ _permissions: { $ne: null } }).fetch()[0]._permissions[CURRENT_USER_ID].read);
+
+      return collection.find({
+        $or: [
+          { _permissions: { [CURRENT_USER_ID]: { read: true } } },
+          { _permissions: { _world_: { read: true } } }
+        ]
+      });
+
     } else {
       return this.ready();
     }
   });
 }
 
-function createPermissionMethods(collection, collectionName) {
-  // add or remove a permissions for a user
-  ['add', 'remove'].forEach(method => {
-    Meteor.methods({
-      [`${collectionName}.${method}Permission`](userId, operation) {
-        checkCollectionPermissions['change collection permissions'](collection, global.CURRENT_USER_ID);
-        collection.update({_id: 'PERMISSIONS'}, {
-          $set: {
-            [`${userId}.${operation}`]: method === 'add'
-          }
-        }, {upsert: true});
-      }
-    });
-
-    collection[`${method}Permission`] = (userId, operation) =>
-        Meteor.call(`${collectionName}.${method}Permission`, userId, operation);
+function createMethods(collection, collectionName) {
+  // add permissions for a user
+  Meteor.methods({
+    [`${collectionName}.addPermission`](userId, operation) {
+      checkCollectionPermissions['change collection permissions'](collection, global.CURRENT_USER_ID);
+      collection.update({_id: 'PERMISSIONS'}, {
+        $set: { [`${userId}.${operation}`]: true }
+      }, { upsert: true });
+    }
   });
+
+  // remove permissions for a user
+  Meteor.methods({
+    [`${collectionName}.removePermission`](userId, operation) {
+      checkCollectionPermissions['change collection permissions'](collection, global.CURRENT_USER_ID);
+      collection.update({_id: 'PERMISSIONS'}, {
+        $unset: { [`${userId}.${operation}`]: false }
+      });
+    }
+  });
+
+  // upsert a document with permission metadata
+  Meteor.methods({
+    [`${collectionName}.save`](model, query, cb) {
+      checkCollectionPermissions.save(collection, global.CURRENT_USER_ID);
+
+      let existing = null;
+
+      if (query) {
+        existing = collection.findOne(query);
+      } else if (model._id) {
+        existing = collection.findOne(model._id);
+      }
+
+      if (existing) {
+        collection.update(existing, model, cb);
+      } else {
+        let modelToSave = model;
+
+        if (!model._permissions) {
+          modelToSave = merge(model, {
+            _permissions: {
+              [CURRENT_USER_ID]: {
+                read: true,
+                save: true,
+                remove: true
+              }
+            }
+          });
+        }
+
+        collection.insert(modelToSave, cb);
+      }
+    }
+  });
+}
+
+function createApi(collection, collectionName) {
+  return {
+    addPermission(userId, operation) {
+      Meteor.call(`${collectionName}.addPermission`, userId, operation);
+    },
+
+    removePermission(userId, operation) {
+      Meteor.call(`${collectionName}.removePermission`, userId, operation);
+    },
+
+    findById(id) {
+      return collection.findOne(id);
+    },
+
+    findOne: collection.findOne,
+
+    find: collection.find,
+
+    findAll() {
+      return collection.find();
+    },
+
+    save(model, query, callback) {
+      Meteor.call(`${collectionName}.save`, model, query, callback);
+    },
+
+    originalCollection: collection
+  }
 }
 
 function createCollection(name, schema) {
@@ -71,27 +145,14 @@ function createCollection(name, schema) {
    */
   collection.schema = schema;
 
-  collection.findById = id => collection.findOne(id);
-  collection.findAll = () => collection.find();
   collection.removeAll = () => collection.remove({});
 
-  collection.save = (model, query) => {
-    let existing = null;
-
-    if (query) {
-      existing = collection.findOne(query);
-    } else if (model._id) {
-      existing = findOne({ _id: model._id });
-    }
-  
-    if (existing) {
-      collection.update(existing, model);
-    } else {
-      collection.insert(model);
-    }
-
-    return collection.findOne(model);
-  };
+  initializePermissions([
+    'read',
+    'remove',
+    'save',
+    'change collection permissions'
+  ], name);
 
   if (Meteor.isServer) {
     setupPublications(collection, name);
@@ -99,9 +160,9 @@ function createCollection(name, schema) {
     Meteor.subscribe(name);
   }
 
-  createPermissionMethods(collection, name);
+  createMethods(collection, name);
 
-  return collection;
+  return createApi(collection, name);
 }
 
 export default createCollection;
